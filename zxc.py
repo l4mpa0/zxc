df0c338e16e8d00427dd330ddf45e8b84e3f0025
Ivan Sentemon <124244721+sentemon@users.noreply.github.com>
2023-11-08 11:05:12 +0100

    Update zxc.py

--- zxc.py
+++ zxc.py
@@ -1,1 +1,47 @@
-print()
+a = int(input('Wpisz pierwszą liczbę: '))
+b = int(input('Wpisz drugą liczbę: '))
+
+x = input('Wybierz operację (+, -, *, /, //, %, **): ')
+
+def calculator(a, b, operator):
+    wynik = None  
+    if operator == '+':
+        wynik = a + b
+    elif operator == '-':
+        wynik = a - b
+    elif operator == '*':
+        wynik = a * b
+    elif operator == '/':
+        if b == 0:
+            print('Nie można dzielić przez zero.')
+            return
+        wynik = a / b
+    elif operator == '//':
+      if b == 0:
+          print('Nie można dzielić przez zero.')
+          return
+      wynik = a // b
+    elif operator == '%':
+        if b == 0:
+            print('Nie można dzielić przez zero.')
+            return
+        wynik = a % b
+    elif operator == '**':
+      wynik = a ** b
+  
+    else:
+        print('Nieprawidłowa operacja. Wybierz spośród: +, -, *, /, //, %, **')
+        return
+
+    print(f'{a} {operator} {b} = {wynik}')
+
+calculator(a, b, x)
+s = input("Czy chcesz kontynuować? (TAK/NIE): ").upper()
+while s == "T" or s == "TAK":
+    a = int(input('Wpisz pierwszą liczbę: '))
+    b = int(input('Wpisz drugą liczbę: '))
+    x = input('Wybierz operację (+, -, *, /, //, %, **): ')
+    calculator(a, b, x)
+    s = input("Czy chcesz kontynuować? (TAK/NIE): ").upper()
+
+print("Poproszę szóstkę :)")
